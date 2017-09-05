d2e33954d635ea210ed6676e7577072362ac3673
KaiserChan <chankaiserkk@gmail.com>
2017-09-05 15:11:38 -0400

    Created reinforcing-exercise - completed exercise

---
+++ reinforcing-exercise.rb
@@ -0,0 +1,17 @@
+key_array = (1..50).to_a
+
+key_array.each do |number|
+  number_hash = {}
+  number_hash[number] = number
+
+  if number % 2 == 0 && number % 7 == 0
+    number_hash[number] = number * 2
+  elsif number % 7 == 0
+    number_hash[number] = number - 1
+  elsif number % 2 == 0
+  number_hash[number] = number + 1
+  else
+    number_hash[number] = number
+  end
+  puts number_hash
+end
